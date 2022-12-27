830da14190dc917d02d2ad5ad01390f589a124b3
Mustafa Hussain <mustafa.hussain93@gmail.com>
2022-12-27 15:20:22 +0100

    use knex to get results

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,19 +1,29 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import * as mysql from 'mysql2/promise';
+import { knex } from 'knex';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
-        const database = await mysql.createConnection({
-            host: 'database-1.coc13ag5l7ap.eu-central-1.rds.amazonaws.com',
-            user: 'admin',
-            password: 'adminpass',
-            database: 'testdb',
+        const database = knex({
+            client: 'mysql2',
+            connection: {
+                host: 'database-1.coc13ag5l7ap.eu-central-1.rds.amazonaws.com',
+                user: 'admin',
+                password: 'adminpass',
+                database: 'testdb',
+            },
         });
+        const result = await database.select().table('vals');
+        // const database = await mysql.createConnection({
+        //     host: 'database-1.coc13ag5l7ap.eu-central-1.rds.amazonaws.com',
+        //     user: 'admin',
+        //     password: 'adminpass',
+        //     database: 'testdb',
+        // });
 
-        const [raws, fields] = await database.execute('SELECT * FROM `vals`');
+        // const [raws, fields] = await database.execute('SELECT * FROM `vals`');
         return {
             statusCode: 200,
-            body: JSON.stringify({ raws, fields }),
+            body: JSON.stringify({ result }),
         };
     } catch (err) {
         console.log(err);
