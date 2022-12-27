beb6c8ace45caf42b7fa90777204b235d72d651c
Mustafa Hussain <mustafa.hussain93@gmail.com>
2022-12-27 15:21:25 +0100

    cleaning

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,26 +1,19 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { knex } from 'knex';
 
+const database = knex({
+    client: 'mysql2',
+    connection: {
+        host: 'database-1.coc13ag5l7ap.eu-central-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'adminpass',
+        database: 'testdb',
+    },
+});
+
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
-        const database = knex({
-            client: 'mysql2',
-            connection: {
-                host: 'database-1.coc13ag5l7ap.eu-central-1.rds.amazonaws.com',
-                user: 'admin',
-                password: 'adminpass',
-                database: 'testdb',
-            },
-        });
         const result = await database.select().table('vals');
-        // const database = await mysql.createConnection({
-        //     host: 'database-1.coc13ag5l7ap.eu-central-1.rds.amazonaws.com',
-        //     user: 'admin',
-        //     password: 'adminpass',
-        //     database: 'testdb',
-        // });
-
-        // const [raws, fields] = await database.execute('SELECT * FROM `vals`');
         return {
             statusCode: 200,
             body: JSON.stringify({ result }),
